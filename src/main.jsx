3633ede030cbe6dd81520fba71f485d91d8c03d9
Leonardo Galindez <88455461+Leonardo-Galindez@users.noreply.github.com>
2024-12-29 03:06:43 -0300

    empezando diseño de nav

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,22 @@ import Container1 from "./components/container-1.jsx";
 import Container2 from "./components/container-2.jsx";
 import Logo from "./components/logo.jsx";
 import SearchBar from "./components/search-bar.jsx";
-import BtnAccount from  "./components/btn-account.jsx";
-import BtnCart from  "./components/btn-cart.jsx";
-
+import BtnAccount from "./components/btn-account.jsx";
+import BtnCart from "./components/btn-cart.jsx";
+import Nav from "./components/nav.jsx";
 /*
   import styles
 */
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <Header
-    container1={<Container1 logo={<Logo />} searchBar={<SearchBar />} />}
-    container2={<Container2 btnAccount={<BtnAccount />} btnCart={<BtnCart />}/>}
-  />
+  <div className="container">
+    <Header
+      container1={<Container1 logo={<Logo />} searchBar={<SearchBar />} />}
+      container2={
+        <Container2 btnAccount={<BtnAccount />} btnCart={<BtnCart />} />
+      }
+    />
+    <Nav />
+  </div>
 );
